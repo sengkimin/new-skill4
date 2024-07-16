5b169bdc1d1349e8e5e34efd837a9c7458c91fd5
sengkimin <sengin.pse@gmail.com>
2024-07-16 16:23:13 +0700

    new page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header(){
     return     <div className="header">
     <div className="title">AgroAdmin</div>
     <div className="profile-icon">
-      <img src="https://via.placeholder.com/40" alt="Profile" />
+      <img src="https://media.discordapp.net/attachments/1257941882248630314/1262692343098900520/vector-farmer-cartoon-character-with-milk-cow-in-organic-rural-farm.png?ex=66978562&is=669633e2&hm=b51eca4a670f267b277135a0bd743755d1019af97a45ed7849c374310277b0a7&=&format=webp&quality=lossless&width=350&height=350" alt="Profile" />
     </div>
   </div>
 }
---
+++ src/pages/Cropcycle.jsx
@@ -0,0 +1,9 @@
+function Cropcycle() {
+    return (
+      <>
+        <h1>Hi Crop Cycle</h1>
+      </>
+    );
+  }
+  
+  export default Cropcycle;
