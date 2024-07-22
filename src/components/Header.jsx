75dfabbbc61bde469b2f53429cbd2f2828a24039
sengkimin <sengin.pse@gmail.com>
2024-07-22 15:50:26 +0700

    pic profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header(){
     return     <div className="header">
     <div className="title">AgroAdmin</div>
     <div className="profile-icon">
-      <img src="https://media.discordapp.net/attachments/1257941882248630314/1262692343098900520/vector-farmer-cartoon-character-with-milk-cow-in-organic-rural-farm.png?ex=669c22a2&is=669ad122&hm=a370e5fef0c7ed3c9e126c6d45f7c58c47c1781c923dd3f4d061529a3c131044&=&format=webp&quality=lossless&width=629&height=629" alt="Profile" />
+      <img src="https://media.discordapp.net/attachments/1257941882248630314/1262692343098900520/vector-farmer-cartoon-character-with-milk-cow-in-organic-rural-farm.png?ex=669ec5a2&is=669d7422&hm=0ba4fa2f67562d133c673b86784d1bbe74696eb6184fc2c37bd8f01f64ffe873&=&format=webp&quality=lossless&width=629&height=629" alt="Profile" />
     </div>
   </div>
 }
